d5b477bf2606a396ebc45357653071d32267fdac
Mohammad Ramish Ansari <ansariramish35@gmail.com>
2026-09-16 20:49:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/1M1B-PROJECT",
+  assetPrefix: "/1M1B-PROJECT/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
